b4afda3842b119b5568634dbda309161852f68d5
david0178418 <davidjgranado@gmail.com>
2012-01-17 23:04:27 -0600

    Corrected "basePath" to "baseUrl"

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
 (function() {
 	"use strict";
+	var libsPath = '../../libs/js/'; //libsPath relative to 'basePath'
 	
 	require.config({
 		paths : {
-			jquery : '../../libs/js/jquery-1.7.1.min',	//return jQuery module but also keeps it in the global scope
-			jqueryLoader : '../../libs/js/jquery',		//loads jQuery module and removes it from the global scope
-			underscore : '../../libs/js/underscore-min',
-			backbone : '../../libs/js/backbone-min'		//
+			jquery : libsPath + 'jquery-1.7.1.min',		//return jQuery module but also keeps it in the global scope
+			jqueryLoader : libsPath + 'jquery',		//loads jQuery module and removes it from the global scope
+			underscore : libsPath + 'underscore-min',
+			backbone : libsPath + 'backbone-min'
 		},
-		basePath : 'src/js'
+		baseUrl : 'src/js'
 	});
 	
 	require([
